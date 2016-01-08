3ea7fa7a72d8dd13dc9ba31ee39c44a14f80bcaf
Ilia Soloveychik <091995_mail@mail.ru>
2016-01-08 22:00:47 +0600

    gismeteo + some more styles!

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
-/*var myModule = (function () {
-
-    var init = function () {
-        // сюда пишем то, что должно произойти сразу. Как правило, это функция "обработчики событий"
-        _setUpListeners();
-
-    };
-
-    var _setUpListeners = function () {
-        $('.header__title').on('click', _goToStart);
-    };
+$(document).ready(function(){
 
 
-    var _goToStart = function(){
-        location.href = '/';
-    };
 
-    /* Все, что пишется до ретурна, нельзя поменять из консоли */
-   /* return {
-        init: init
-    };
-
-})();
-
-myModule.init();*/
+});
