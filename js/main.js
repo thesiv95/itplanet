f50e84bb57eaeb20ecca5693d67b4ab23a8261df
Ilia Soloveychik <091995_mail@mail.ru>
2016-01-08 13:38:35 +0600

    add other pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-/**
- * Created by User on 05.01.2016.
- */
-$(document).ready(function(){
-    console.log('js rabotaet');
-});
+/*var myModule = (function () {
+
+    var init = function () {
+        // сюда пишем то, что должно произойти сразу. Как правило, это функция "обработчики событий"
+        _setUpListeners();
+
+    };
+
+    var _setUpListeners = function () {
+        $('.header__title').on('click', _goToStart);
+    };
+
+
+    var _goToStart = function(){
+        location.href = '/';
+    };
+
+    /* Все, что пишется до ретурна, нельзя поменять из консоли */
+   /* return {
+        init: init
+    };
+
+})();
+
+myModule.init();*/
